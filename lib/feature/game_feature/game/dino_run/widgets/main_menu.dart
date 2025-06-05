036c1a14f39ee5abb4cb04ba3a32f91395dea79a
Chuyn2k3 <phamdoan060801@gmail.com>
2025-06-05 22:24:15 +0700

    update

--- lib/feature/game_feature/game/dino_run/widgets/main_menu.dart
+++ lib/feature/game_feature/game/dino_run/widgets/main_menu.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flame/flame.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:fun_edu/feature/game_feature/game/dino_run/game/audio_manager.dart';
@@ -22,54 +23,51 @@ class MainMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-        child: Card(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          color: Colors.black.withAlpha(100),
-          child: FittedBox(
-            fit: BoxFit.scaleDown,
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
-              child: Wrap(
-                direction: Axis.vertical,
-                crossAxisAlignment: WrapCrossAlignment.center,
-                spacing: 10,
-                children: [
-                  const Text(
-                    'Khủng Long thoát hiểm',
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        color: Colors.black.withAlpha(100),
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 100),
+            child: Wrap(
+              direction: Axis.vertical,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              spacing: 10,
+              children: [
+                const Text(
+                  'Khủng Long thoát hiểm',
+                  style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.white,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    game.startGamePlay();
+                    game.overlays.remove(MainMenu.id);
+                    game.overlays.add(Hud.id);
+                  },
+                  child: const Text(
+                    'Chơi',
                     style: TextStyle(
-                      fontSize: 50,
-                      color: Colors.white,
+                      fontSize: 30,
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      game.startGamePlay();
-                      game.overlays.remove(MainMenu.id);
-                      game.overlays.add(Hud.id);
-                    },
-                    child: const Text(
-                      'Chơi',
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    game.overlays.remove(MainMenu.id);
+                    game.overlays.add(SettingsMenu.id);
+                  },
+                  child: const Text(
+                    'Cài đặt',
+                    style: TextStyle(
+                      fontSize: 30,
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      game.overlays.remove(MainMenu.id);
-                      game.overlays.add(SettingsMenu.id);
-                    },
-                    child: const Text(
-                      'Cài đặt',
-                      style: TextStyle(
-                        fontSize: 30,
-                      ),
-                    ),
-                  ),
+                ),
+                if (!kIsWeb)
                   ElevatedButton(
                     onPressed: () {
                       // Quay lại chế độ dọc trước khi chuyển màn
@@ -80,7 +78,7 @@ class MainMenu extends StatelessWidget {
                       Flame.device.setPortrait();
                       AudioManager.instance.stopBgm();
                       context.pop(context);
-// Chuyển sang màn hình khác
+                      // Chuyển sang màn hình khác
                       // Navigator.pushReplacement(
                       //   context,
                       //   MaterialPageRoute(
@@ -95,8 +93,7 @@ class MainMenu extends StatelessWidget {
                       ),
                     ),
                   ),
-                ],
-              ),
+              ],
             ),
           ),
         ),
--- lib/feature/game_feature/game/sweep/game/game/overlay_widgets/main_menu.dart
+++ lib/feature/game_feature/game/sweep/game/game/overlay_widgets/main_menu.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fun_edu/feature/game_feature/game/sweep/game/data/providers/score_provider.dart';
@@ -70,7 +71,7 @@ class MainMenu extends ConsumerWidget {
                   },
                 ),
                 const SizedBox(height: 30),
-                const LogoutButton(),
+                if (!kIsWeb) const LogoutButton(),
                 const SizedBox(
                   height: 40,
                 ),
